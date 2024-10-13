c100abe0cb8ed12128697f82381156c3e44959e5
Lowky51 <allanfleaux3@gmail.com>
2024-10-13 15:52:36 +0200

    Add files via upload

---
+++ javascript/framwork_.js
@@ -0,0 +1,20 @@
+function windows_alert() {
+    window.alert("ALERTE");
+}
+
+function button_print() {
+    window.print()
+}
+
+var print_pdf = {
+    "printArea": "%printer%",
+    "printBackground": true,
+    "printPages": {
+      "Range": "all"
+    }
+  };
+   
+  // Ajouter les paramètres d'impression au PDF
+  app.addEventListener("pageshow", function() {
+    app.print(print_pdf);
+  });

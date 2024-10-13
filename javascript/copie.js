e97663c4273413a451a27ddd5499c453980dd45c
Lowky51 <allanfleaux3@gmail.com>
2024-10-13 15:55:38 +0200

    Add files via upload

---
+++ javascript/copie.js
@@ -0,0 +1,94 @@
+function copierTexte_1() {
+    const p = document.getElementById('texteACopier_1');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+
+function copierTexte_2() {
+    const p = document.getElementById('texteACopier_2');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+function copierTexte_3() {
+    const p = document.getElementById('texteACopier_3');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+function copierTexte_4() {
+    const p = document.getElementById('texteACopier_4');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+
+
+
+
+
+function copierTexte_5() {
+    const p = document.getElementById('texteACopier_5');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+
+function copierTexte_6() {
+    const p = document.getElementById('texteACopier_6');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+function copierTexte_7() {
+    const p = document.getElementById('texteACopier_7');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
+
+function copierTexte_8() {
+    const p = document.getElementById('texteACopier_8');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(p);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges(); // Désélectionner le texte
+}
